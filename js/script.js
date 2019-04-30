664d119ee7e4138831a47de96a14b8c5d042769d
gadzhi <gadzhidadaev95@gmail.com>
2019-04-30 13:44:57 +0300

    fix styles bugs and add input mask

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 document.addEventListener('DOMContentLoaded', function(){
+
+  // scroll to form
   document.querySelector('.header_inner a').onclick = function(e) {
     e.preventDefault()
     document.querySelector('.form').scrollIntoView({
       behavior: "smooth"
     })
+  }
+
+  // phone mask
+  document.querySelector('input[name="phone"]').oninput = function() {
+    this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+  }
+  document.querySelector('input[name="phone"]').onchange = function() {
+    let pat = this.value.replace(/\D/g, '').match(/(\d{1})(\d{3})(\d{3})(\d{2})(\d{2})/)
+    this.value = '+' + pat[1] + ' (' + pat[2] + ') ' + pat[3] + '-' + pat[4] + '-' + pat[5]
   }
 
 });
